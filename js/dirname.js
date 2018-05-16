994bccaa44bfaf8d99a4a2ac2152856f9a797974
关晓丹 <GXD9983@163.com>
2018-05-16 18:27:20 +0800

    js 完成

--- js/dirname.js
+++ js/dirname.js
@@ -1,7 +1,7 @@
 /**
  * Created by GXD on 2018/3/22.
- *
+ * 获取文件或所在文件夹的绝对路径
  */
 'use strict';
-console.log('__dirname', __dirname)
-console.log('__filename', __filename)
+console.log('__dirname:', __dirname) // __dirname: /Users/guanxiaodan/WebstormProjects/gxd/myTest/html-css-jsTest/js
+console.log('__filename:', __filename) // __filename: /Users/guanxiaodan/WebstormProjects/gxd/myTest/html-css-jsTest/js/dirname.js
