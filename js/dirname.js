e8dea938ea1afad5f01af221ea10878ae7455552
关晓丹 <GXD9983@163.com>
2019-06-20 22:27:15 +0800

    再次尝试

--- js/dirname.js
+++ js/dirname.js
@@ -3,5 +3,5 @@
  * 获取文件或所在文件夹的绝对路径
  */
 'use strict';
-console.log('__dirname:', __dirname) // __dirname: /Users/guanxiaodan/WebstormProjects/gxd/myTest/html-css-jsTest/js
+console.log('__dirname:', '__dirname') // __dirname: /Users/guanxiaodan/WebstormProjects/gxd/myTest/html-css-jsTest/js
 console.log('__filename:', __filename) // __filename: /Users/guanxiaodan/WebstormProjects/gxd/myTest/html-css-jsTest/js/dirname.js
